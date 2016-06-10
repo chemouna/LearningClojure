ff5f7a967c97d1137201b9570cd2714ec4f1d3e5
Mouna Cheikhna <mouna.cheikhna@blablacar.com>
2016-06-11 01:59:34 +0200

    Add split-with-size.

--- learning/src/learning/core.clj
+++ learning/src/learning/core.clj
@@ -1,23 +1,17 @@
 (ns learning.core)
 
-(defn -main
- []
- (println "Hello...")
-)
-
-;(defn isPalindrome? [s]
-;  (= s (apply str (reverse s) )))
-
-;(defn -main
-;  [& args]
-;  (println "Started isPalindrome Calculation...")
-;  (print " : ")
-;  (println (isPalindrome? 9994))
-;  )
-
-
 ;; takes a list and a nb and adds it to each element 
 (defn list+ [lst a]
   (map #(+ % a) lst))
 
-(list+ '(1 2 3) 10)
+
+(defn split-with-size
+  [[v & vs] s]
+  (if-not v
+    (list s)
+    (cons (take v s) (split-with-size vs (drop v s)))))
+
+(split-with-size [1 3 2 2] '(a b c d e f g h l m))
+
+
+
