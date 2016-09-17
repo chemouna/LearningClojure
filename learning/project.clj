80f0d21ed74937e690a94e5af2c15596f757bd09
Mouna Cheikhna <cheikhnamouna@gmail.com>
2016-09-17 11:31:48 +0200

    macro try catch.

--- learning/project.clj
+++ learning/project.clj
@@ -3,6 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8b.0"]
-                 [org.clojure/math.numeric-tower "0.0.4"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/math.numeric-tower "0.0.4"]
+                 [com.taoensso/timbre "4.7.4"]]
   :main learning.core)
---
+++ learning/src/learning/exploremacros.clj
@@ -0,0 +1,42 @@
+(ns learning.exploremacros)
+
+(defmacro try-catch-default
+  [default-value operation]
+  `(try
+     ~operation
+     (catch Exception x#
+       (println "Error occured: " x#  ", defaults to: " ~default-value)
+            ~default-value)))
+
+(macroexpand-1 `(try-catch-default 10 (/ 1 4)))
+
+(try-catch-default 10 (/ 1 4))
+
+(defn load-default-value
+  []
+  (println "loading default value")
+  (comment loading from db)
+  3)
+
+(macroexpand-1 '(try-catch-default (load-default-value) (/ 1 0)))
+
+(try-catch-default (load-default-value) (/ 1 0))
+
+;; load-default-value called twice
+;; let's prevent that
+
+(defmacro try-catch-default2
+  [default-value operation]
+  `(try
+     ~operation
+     (catch Exception x#
+       (let [default# ~load-default-value]
+         (println "Error occured : " x# ", defaults to : " default#)
+         default#)
+       )))
+
+(macroexpand-1 '(try-catch-default2 (load-default-value) (/ 1 0)))
+
+(try-catch-default2 (load-default-value) (/ 1 0))
+
+
