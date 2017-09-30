17664796ca47c1716c4447a4b49bc6405147d896
Mouna Cheikhna <cheikhnamouna@gmail.com>
2017-09-30 11:22:54 +0100

    On function.

--- learning2/src/learning2/on.clj
+++ learning2/src/learning2/on.clj
@@ -1,13 +1,16 @@
 (ns learning2.on)
 
 (defn on
-  [f g]
-  (fn [x y]
-    (g (f x) (f y)) ))
+  ([f g]
+     (fn [x y]
+       (f (g x)
+          (g y))))
+  ([f g & args]
+     (on f #(apply g % args))))
 
 (defn anagram?
   [word other-word]
   (and (not= word other-word)
        ((on = sort) word other-word)))
 
-(anagram? "t" "d")
+(anagram? "nagaram" "anagram")
