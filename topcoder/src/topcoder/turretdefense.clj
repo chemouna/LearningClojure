ef6b45ba59dcd6cc624c9a8a7fb2aa69f1d9f30c
Mouna Cheikhna <cheikhnamouna@gmail.com>
2017-10-18 22:47:00 +0100

    Finding a solution.

--- topcoder/src/topcoder/turretdefense.clj
+++ topcoder/src/topcoder/turretdefense.clj
@@ -13,7 +13,24 @@
 
 (def xs '(3 5 6))
 (def ys '(7 5 6))
+(def ts '(11 15 16))
 (diff-with-itself xs)
 (def diffX (diff-with-itself xs))
 (def diffY (diff-with-itself ys))
-;; (def timeNeed (map + diffX diffY))
+(def timeNeed (map + diffX diffY))
+(def tn (cons 0 (map + timeNeed (butlast ts))))
+
+(defn first-index
+  ([pred coll] (first-index coll pred 0))
+  ([pred coll idx]
+   (cond (= coll '()) -1
+         (pred (first coll)) idx
+         :else (recur pred (rest coll) (inc idx)))))
+
+(def l (map - '(11 15 16) '(0 15 17)))
+
+(defn is-neg? [x] (< x 0))
+
+(first-index is-neg? l)
+
+;;(first-index #((first %1) > (first (rest %1))) l)
